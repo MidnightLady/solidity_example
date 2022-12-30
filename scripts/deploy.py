f69e4f8733697c3d276fde4ab863ddc33de865b8
Thành Nguyễn <ThanhNT@coin98.finance>
2022-12-30 09:30:52 +0700

    update

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,14 +1,14 @@
 from brownie import *
 
+# accounts.add(private_key="")
+#
 
 def main():
     a0 = accounts[0]; a1 = accounts[1]; a2 = accounts[2]
     # requires brownie account to have been created
     # if network.show_active() == 'development':
     #
-    # add these accounts to metamask by importing private key
-    # accounts.add(private_key="0x3662a22892fc295c76adb33fc89ee7c8e1cdf769d7e059d28d0bed0d659eeaf9")
-    #
+
     # owner = accounts[0]
     # owner.transfer(accounts[-1], "10 ether")
 
